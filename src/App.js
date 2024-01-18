1ffd6135a4d8ca38d209d896a93e980180ce18bf
Rishikesh1601 <rushikeshmundada1601@gmail.com>
2024-01-18 17:07:03 +0530

    removes all the data from the app.js and added to home.page.js and made a route to '/'

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
-
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Navbar from './components/Navbar';
-import MainTitle from './components/MainTitle';
-import EndToEndComponent from './components/Endtoendproducts.component';
-import AboutusComponent from './components/aboutus.component';
-import CounterComponent from './components/Counter.component';
-import CompaniesSlider from './components/Companiesslider';
-import TeamAgumentation from './components/Teamagumentation';
-import OurProjectComponent from './components/OurProject.component';
-import Footer from './components/Footer.component';
+import HomePage from './page/home.page';
+
 function App() {
   return (
     <>
-    <div className=''>
-      <Navbar />
-      <MainTitle />
-      <AboutusComponent />
-      <br></br>
-      <br></br>
-      <EndToEndComponent />
-      <br></br>
-      <br></br>
-      <TeamAgumentation />
-      <br></br>
-      <br></br>
-      <CounterComponent />
-      <br></br>
-      <br></br>
-      <OurProjectComponent />
-      <br></br>
-      <br></br>
-      <CompaniesSlider />
-      <br></br>
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
     </>
   );
 }
--- src/page/home.page.js
+++ src/page/home.page.js
@@ -1,13 +1,37 @@
 import React from 'react'
 import Navbar from '../components/Navbar';
 import MainTitle from '../components/MainTitle';
+import AboutusComponent from '../components/aboutus.component';
+import EndToEndComponent from '../components/Endtoendproducts.component';
+import TeamAgumentation from '../components/Teamagumentation';
+import CounterComponent from '../components/Counter.component';
+import OurProjectComponent from '../components/OurProject.component';
+import CompaniesSlider from '../components/Companiesslider';
+import Footer from '../components/Footer.component';
 
 const HomePage = () => {
   return (
     <>
     <Navbar />
     <MainTitle />
-    
+    <AboutusComponent />
+    <br></br>
+    <br></br>
+    <EndToEndComponent />
+    <br></br>
+    <br></br>
+    <TeamAgumentation />
+    <br></br>
+    <br></br>
+    <CounterComponent />
+    <br></br>
+    <br></br>
+    <OurProjectComponent />
+    <br></br>
+    <br></br>
+    <CompaniesSlider />
+    <br></br>
+    <Footer />
     </>
   )
 }
