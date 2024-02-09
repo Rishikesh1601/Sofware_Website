21d8cf0539d63789e4461060d823e1376b70ac7c
Rishikesh1601 <rushikeshmundada1601@gmail.com>
2024-02-09 15:49:07 +0530

    hero for main title edited: added image and scroll down arrow animation

--- src/components/MainTitle.js
+++ src/components/MainTitle.js
@@ -1,6 +1,6 @@
 import React, {useState,useEffect} from "react";
 import './navbar.css'
-import { ReactComponent as Heropng } from '../assets/heropage.svg';
+import { ReactComponent as Heropng } from '../assets/Images/main svg.svg';
 const MainTitle = () => {
     const [dynamicText, setDynamicText] = useState('Web Development');
   const textArray = ['Enterprise Mobility', 'Software Support', 'API Integration'];
@@ -34,9 +34,12 @@ const MainTitle = () => {
                     <a class="themeht-btn" href="/career">Get Started</a>
                     <a class="themeht-btn dark-btn ms-3" href="/contactus">Contact Us</a>
                     </div>
+                    <div class="scroll-down"></div>
                 </div>
+                
                 </div>
             </div>
+            
             <div class="wave-shape">
                 <svg width="100%" height="150px" fill="none">
                 <path fill="#ffffff">
@@ -46,8 +49,10 @@ const MainTitle = () => {
                     M0 53.6584C158 11.0001 213 0 363 0C513 0 855.555 115.001 1154 115.001C1440 115.001 1626 -38.0004 2560 53.6585V199.66L0 199.66V53.6584Z;
                     M0 25.9086C277 84.5821 433 65.736 720 25.9086C934.818 -3.9019 1214.06 -5.23669 1442 8.06597C2079 45.2421 2208 63.5007 2560 25.9088V171.91L0 171.91V25.9086Z"></animate>
                 </path>
+                
                 </svg>
             </div>
+
             </section>
 
         </>
--- src/page/home.page.js
+++ src/page/home.page.js
@@ -16,6 +16,7 @@ const HomePage = () => {
     <>
     <Navbar />
     <MainTitle />
+    <br></br>
     <AboutusComponent />
     <br></br>
     <br></br>
